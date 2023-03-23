2a34469933f60aec7250dfabf90e522101de41cd
jethanstory <32407746+jethanstory@users.noreply.github.com>
2023-03-23 15:44:45 -0700

    Added typing test component

--- Project-HandsFree/Assets/Scripts/TextingInputScr.cs
+++ Project-HandsFree/Assets/Scripts/TextingInputScr.cs
@@ -6,8 +6,10 @@ public class TextingInputScr : MonoBehaviour
 {
     public GameObject textTestSentCanvas;
     public GameObject textTestRecievedCanvas;
+    public GameObject textTestTypingCanvas;
     public GameObject textRecievedSound;
     public GameObject textSentSound;
+    public GameObject textTypeSound;
     public GameObject fpsPlayer;
 
     public bool hasResponded = false;
@@ -15,11 +17,14 @@ public class TextingInputScr : MonoBehaviour
 
     public float selectedRandNum;
     public float countdownNum;
+    public int maxType;
+    public int currentType = 0;
     // Start is called before the first frame update
     void Start()
     {
         textTestRecievedCanvas.SetActive(true);
         selectedRandNum = Random.Range(3f, 20.0f);
+        maxType = Random.Range(1,20);
     }
 
     // Update is called once per frame
@@ -31,16 +36,27 @@ public class TextingInputScr : MonoBehaviour
         {
             // selectedRandNum = Random.Range(0f, 50.0f);
             selectedRandNum = Random.Range(0f, 10.0f);
-            textSentSound.SetActive(false);
-            textSentSound.SetActive(true);
-            countdownNum = 0f;
+            textTypeSound.SetActive(false);
+            textTypeSound.SetActive(true);
             textTestRecievedCanvas.SetActive(false);
-            textTestSentCanvas.SetActive(true);
-            hasResponded = true;
+            textTestTypingCanvas.SetActive(true);
+            countdownNum = 0f;
+            currentType++;
+            if (currentType >= maxType)
+            {
+                textSentSound.SetActive(false);
+                textSentSound.SetActive(true);
+                hasResponded = true;
+                currentType = 0;
+                textTestTypingCanvas.SetActive(false);
+                textTestSentCanvas.SetActive(true);
+            }
+
         }
 
         if (countdownNum > selectedRandNum && hasResponded)
         {
+            maxType = Random.Range(1,20);
             // selectedRandNum = Random.Range(0f, 50.0f);
             textRecievedSound.SetActive(false);
             textRecievedSound.SetActive(true);
