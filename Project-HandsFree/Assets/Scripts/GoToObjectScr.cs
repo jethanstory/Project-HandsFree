945e109596d1e8152de34e29e907656a5f06a327
jethanstory <32407746+jethanstory@users.noreply.github.com>
2023-03-26 13:02:53 -0700

    Added early navmesh car tester

---
+++ Project-HandsFree/Assets/Scripts/GoToObjectScr.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class GoToObjectScr : MonoBehaviour
+{
+    public float damping;
+
+    public NavMeshAgent agent;
+    //public NavMeshAgent fpsTarget;
+    public Transform targetObject;
+
+    // Start is called before the first frame update
+
+    [Range(0, 100)] public float speed;
+    [Range(1, 500)] public float walkRadius;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        if (agent != null)
+        {
+            agent.speed = speed;
+            agent.SetDestination(targetObject.position);
+        }
+    }
+
+    /*public Vector3 RandomNavMeshLocation()
+    {
+        Vector3 finalPosition = Vector3.zero;
+        Vector3 randomPosition = Random.insideUnitSphere * walkRadius;
+        randomPosition += transform.position;
+        if (NavMesh.SamplePosition(randomPosition, out NavMeshHit hit, walkRadius, 1)) {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
+    } */
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rotation = Quaternion.LookRotation(targetObject.position - transform.position);
+        transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * damping);
+        //float distance = Vector3.Distance(player.transform.position,transform.position);
+        //if (agent != null && agent.remainingDistance <= agent.stoppingDistance)
+        //{
+            
+            //agent.SetDestination(RandomNavMeshLocation());
+        //}
+        transform.LookAt(targetObject);
+        agent.SetDestination(targetObject.position);
+        
+
+    }
+}
